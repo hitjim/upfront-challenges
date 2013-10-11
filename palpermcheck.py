dd1af9fd34192171c2325018a5bb688d458a3038
Jim Rice <hitjim@gmail.com>
2013-10-11 01:26:03 -0500

    some pseudocode

--- palpermcheck.py
+++ palpermcheck.py
@@ -1,1 +1,16 @@
+#! /usr/bin/env python
+
+# hitjim@gmail.com
+# palindrome permutation checker
+# http://www.upfrontwichita.com/challenges/2
+#
+# usage guide:
+# use it
+
 import sys
+
+
+
+# Deal with short-circuits first
+# If odd
+# If >1 letter which occurs an odd number of times
